0fffa4e54f49d297183d0d543fa7af89026d7fdd
ladiel <100488576+ladiel86@users.noreply.github.com>
2023-09-25 16:27:16 +0200

    Deleted commented helper code

--- scripts/navigate-samples.js
+++ scripts/navigate-samples.js
@@ -18,26 +18,6 @@ document.querySelectorAll(".sample-link").forEach(item => {
             if (sample.getAttribute("data-title") == clickedButton) sample.removeAttribute("hidden")
             else sample.setAttribute("hidden", "");
         }
-
-        // let outroID = document.querySelector("[selected").id;
-        // let outroObjects = document.getElementsByClassName(outroID);
-        // let outroObjectsID = [];
-        // for (let element of outroObjects) outroObjectsID.push(element.id);
-        
-        // let introID = event.target.id;
-        // let introObjects = document.getElementsByClassName(introID);
-        // let introObjectsID = [];
-        // for (let element of introObjects) introObjectsID.push(element.id);
-
-        // if (introObjectsID.length == outroObjectsID.length && introObjectsID.length == 2) {
-        //     introObjectsID.pop();
-        //     outroObjectsID.pop();
-        // }
-        // outroObjectsID.forEach((object) => transition(object, 0));
-        // introObjectsID.forEach((object) => transition(object, 1));
-
-        // document.getElementById(outroID).removeAttribute("selected");
-        // document.getElementById(introID).setAttribute("selected", "");
     })
 
 })
